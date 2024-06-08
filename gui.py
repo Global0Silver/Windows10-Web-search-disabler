518dec12348b8289bd5f3997e5e2adb2c65e1547
Global silver <87803221+Global0Silver@users.noreply.github.com>
2024-06-08 17:31:13 +0300

    Fixed and working

--- gui.py
+++ gui.py
@@ -2,15 +2,15 @@ import tkinter
 import customtkinter
 import winreg
 
-#Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search
-REG_PATH = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
+
+REG_PATH = r"SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings"
 
 def set_reg(name, value):
     try:
         winreg.CreateKey(winreg.HKEY_CURRENT_USER, REG_PATH)
         registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0, 
                                        winreg.KEY_WRITE)
-        winreg.SetValueEx(registry_key, name, 0, winreg.REG_SZ, value)
+        winreg.SetValueEx(registry_key, name, 0, winreg.REG_DWORD, value)
         winreg.CloseKey(registry_key)
         return True
     except WindowsError:
@@ -20,7 +20,7 @@ def get_reg(name):
     try:
         registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0,
                                        winreg.KEY_READ)
-        value, regtype = winreg.QueryValueEx(registry_key, name)
+        value, regtype = winreg.REG_DWORD(registry_key, name)
         winreg.CloseKey(registry_key)
         return value
     except WindowsError:
@@ -28,10 +28,10 @@ def get_reg(name):
     
 
 def enable_ws():
-    set_reg('BingSearchEnabled', str(1))
+    set_reg('IsGlobalWebSearchProviderToggleEnabled', 1)
     finish_label.configure(text="Web search enabled")
 def disable_ws():
-    set_reg('BingSearchEnabled', str(0))
+    set_reg('IsGlobalWebSearchProviderToggleEnabled', 0)
     finish_label.configure(text="Web search disabled")
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import winreg
-#Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search
-REG_PATH = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
+#Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings
+REG_PATH = r"SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings"
 
 def set_reg(name, value):
     try:
         winreg.CreateKey(winreg.HKEY_CURRENT_USER, REG_PATH)
         registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0, 
                                        winreg.KEY_WRITE)
-        winreg.SetValueEx(registry_key, name, 0, winreg.REG_SZ, value)
+        winreg.SetValueEx(registry_key, name, 0, winreg.REG_DWORD, value)
         winreg.CloseKey(registry_key)
         return True
     except WindowsError:
@@ -22,11 +22,15 @@ def get_reg(name):
         return value
     except WindowsError:
         return None
-print ("")
-input ("Disable web search > 1 \n Enable web search > 2")
-set_reg('BingSearchEnabled', str(1))
+print ("Disable web search > 1")
+print ("Enable web search  > 2")
+user_choice = input (">")
+if user_choice == "1":
+ set_reg('IsGlobalWebSearchProviderToggleEnabled', 0)
+else:
+ set_reg('IsGlobalWebSearchProviderToggleEnabled', 1)
 #Read value and double check    
-if (get_reg('BingSearchEnabled')) == 1:
+if (get_reg('IsGlobalWebSearchProviderToggleEnabled')) == 0:
     print ("Web search disabled")
 else:
     print ("Web search enabled")
